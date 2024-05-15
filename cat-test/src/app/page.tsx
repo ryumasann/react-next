f485c54f92c2974ad1c8184052a0a67f233e2231
ryumasann <im20140319@gmail.com>
2024-05-16 00:09:09 +0900

    ✅ onClick

--- cat-test/src/app/page.tsx
+++ cat-test/src/app/page.tsx
@@ -1,8 +1,14 @@
+"use client";
 import Image from "next/image";
 import { Cat } from '../../components/cat';
 import { TodoList } from '../../components/todo';
 
+const handleClick = () => {
+  alert('送信しました！');
+};
+
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -12,6 +18,7 @@ export default function Home() {
         </p>
         <Cat />
         <TodoList />
+        <button onClick={handleClick}>送信する</button>
         {/* <Cat width="250px" height="250px"/> */}
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
